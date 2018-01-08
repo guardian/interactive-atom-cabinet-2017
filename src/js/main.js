9a89650a0aee372f31a29dcb791f588037773649
Feilding Cage <feilding.cage@gmail.com>
2018-01-08 08:45:11 +0000

    style updates, adds click btn

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
 var el = document.createElement('script');
 el.src = '<%= path %>/app.js';
 document.body.appendChild(el);
+
+var btn = document.querySelector('.expand-btn')
+var div = document.querySelector('.chart-wrapper')
+btn.addEventListener('click', function(){
+
+  div.setAttribute('class', 'chart-wrapper')
+  btn.style.display = "none";
+  window.resize();
+
+})
